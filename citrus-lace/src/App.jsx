eb68c8b91b729a9cec14f9beee3c1fb7afc422cd
Kenneth Bakke <kennethsbakke@gmail.com>
2022-03-22 12:35:42 -0700

    Fixed bug where page would not update on deleting recipe

--- citrus-lace/src/App.jsx
+++ citrus-lace/src/App.jsx
@@ -7,90 +7,96 @@ import Sidebar from './Sidebar/Sidebar.jsx';
 import items from './Sidebar/variables.js';
 import AppContext from './AppContext.js';
 
-const baseURL = 'http://localhost:8000/recipe'
+const baseURL = 'http://localhost:8000/recipe';
 
-export default function App () {
+export default function App() {
   const [recipes, setRecipes] = useState([]);
   const [selectedRecipe, setSelectedRecipe] = useState({});
   const [system, setSystem] = useState('Metric');
   const [view, setView] = useState('Form');
   const csrftoken = getCookie('csrftoken');
-  const views = ['Form', 'Recipe']
+  const views = ['Form', 'Recipe'];
 
   useEffect(() => {
     let clearId = setTimeout(() => {
       getRecipes();
-    }, 100)
+    }, 100);
 
     return () => {
       clearTimeout(clearId);
-    }
-  }, [])
+    };
+  }, []);
 
   const getRecipes = () => {
     fetch(`${baseURL}/recipes`)
-    .then(res => res.json())
-    .then(data => {
-      setRecipes(data)
-    })
-    .catch(err => console.error(err));
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        setRecipes(data);
+      })
+      .catch((err) => console.error(err));
+  };
 
   const selectRecipe = (recipe) => {
     setSelectedRecipe(recipe);
-  }
+  };
 
   const postRecipe = async (recipe) => {
     fetch(`${baseURL}/recipes/`, {
-        method: 'POST',
-        cache: 'no-cache',
-        referrerPolicy: 'no-referrer',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'X-CSRFToken': csrftoken
-        },
-        body: JSON.stringify(recipe)
-      })
-      .then(res => {
+      method: 'POST',
+      cache: 'no-cache',
+      referrerPolicy: 'no-referrer',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken,
+      },
+      body: JSON.stringify(recipe),
+    })
+      .then((res) => {
         getRecipes();
         selectRecipe(recipe);
-        setView(views[1])
+        setView(views[1]);
       })
-      .catch(err => console.error(err));
+      .catch((err) => console.error(err));
+  };
 
-  }
-
-  const deleteRecipe = (id) => {
+  const deleteRecipe = async (id) => {
     fetch(`${baseURL}/recipes/`, {
       method: 'DELETE',
       cache: 'no-cache',
       referrerPolicy: 'no-referrer',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json',
-        'X-CSRFToken': csrftoken
+        'X-CSRFToken': csrftoken,
       },
-      body: JSON.stringify(id)
+      body: JSON.stringify(id),
     })
-    .then(getRecipes())
-    .catch(err => console.error(err));
-  }
+      .then(() => {
+        getRecipes();
+        setView(views[0]);
+      })
+      .catch((err) => console.error(err));
+  };
 
   return (
     <Fragment>
-      <AppContext.Provider value={{
-        recipesContext: [recipes, setRecipes],
-        selectedRecipeContext: [selectedRecipe, setSelectedRecipe],
-        systemContext: [system, setSystem],
-        viewContext: [view, setView]
-      }}>
+      <AppContext.Provider
+        value={{
+          recipesContext: [recipes, setRecipes],
+          selectedRecipeContext: [selectedRecipe, setSelectedRecipe],
+          systemContext: [system, setSystem],
+          viewContext: [view, setView],
+        }}
+      >
         <NavHeader />
-        <div className="ui container" >
+        <div className='ui container'>
           <Sidebar items={items} />
-          {view === 'Form' ? <RecipeForm postRecipe={postRecipe} />
-          : view === 'Recipe' ? <RecipeView deleteRecipe={deleteRecipe} /> : null
-          }
+          {view === 'Form' ? (
+            <RecipeForm postRecipe={postRecipe} />
+          ) : view === 'Recipe' ? (
+            <RecipeView deleteRecipe={deleteRecipe} />
+          ) : null}
         </div>
       </AppContext.Provider>
     </Fragment>
